f4c3ff4168b6e2162f1d5e615dac3e2c75210464
Anneke Muir <prq9377@autuni.ac.nz>
2025-07-11 01:53:52 +1200

    Store tokens, create test event function
    Created auth callback where tokens are logged in terminal
    Refresh token to be added to env
    Created working create-event function with test values

--- backend/index.js
+++ backend/index.js
@@ -27,4 +27,52 @@ app.get('/auth', (req, res) => {
     res.redirect(url);
 })
 
+app.get('/auth/callback', async (req, res) => {
+    const code = req.query.code;
+
+    try {
+        const {tokens} = await oAuth2Client.getToken(code); // exchange code for tokens
+        oAuth2Client.setCredentials(tokens);
+        console.log('Tokens acquired:', tokens);
+        res.send('Authentication successful! Check terminal for tokens.');
+    } catch (error) {
+        console.error('Error retrieving access token', error);
+        res.status(500).send('Error retrieving access token');
+    }
+})
+
+app.get('/create-event', async (req, res) => {
+    try {
+        oAuth2Client.setCredentials({
+            refresh_token: process.env.REFRESH_TOKEN // use the refresh token to set credentials
+        });
+
+        const calendar = google.calendar({version: 'v3', auth: oAuth2Client});
+
+        const event = {
+            summary: 'Test Event',
+            description: 'Created with Google Calendar API :D',
+            start: {
+                dateTime: '2025-07-10T10:00:00',
+                timeZone: 'Pacific/Auckland',
+            },
+            end: {
+                dateTime: '2025-07-10T16:00:00',
+                timeZone: 'Pacific/Auckland',
+            },
+            colorId: '11' // Tomato colour as default because I like it
+        };
+
+        const response = await calendar.events.insert({
+            calendarId: process.env.CALENDAR_ID || 'primary', // use specific calendar ID otherwise use primary
+            resource: event,
+        })
+
+        res.send(`Event created: <a href="${response.data.htmlLink}">${response.data.htmlLink}</a>`);
+    } catch (error) {
+        console.error('Error creating event:', error);
+        res.status(500).send('Error creating event');
+    }
+})
+
 app.listen(5000, () => console.log('Backend running on http://localhost:5000')); 
